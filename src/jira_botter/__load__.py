c2a228108c6093fd9e4d3efcad0ccf4f7563c736
BMLN <lorandbanki@gmail.com>
2026-01-02 14:10:35 +0100

    jira_botter.__encode__ misc

--- src/jira_botter/__load__.py
+++ src/jira_botter/__load__.py
@@ -66,31 +66,12 @@ def processor(data, kb):
 
 
 
-if __name__ == "__main__":
+def load(files, host, port, collection, batch_size, designated_load, designated_bytes, error_threshold):
     
-    #configuration
-    args = ArgumentParser("load")
-    args.add_argument("--host", action="store", type=str, required=True)
-    args.add_argument("--port", action="store", type=str, required=True)
-    args.add_argument("--collection", action="store", type=str, required=True)
-    args.add_argument("files", action="store", nargs="+", type=str)
-
-    args.add_argument("--batch_size", action="store", type=int, default=None)
-    args.add_argument("--designated_load", action="store", type=float, default=0.25)
-    args.add_argument("--designated_bytes", action="store", type=int, default=500000000)
-    args.add_argument("--error_threshold", action="store", type=float, default=0.8)
-    
-    args = args.parse_args()
-
-
-
-
-
-
     #connection
     try:
-        conn = WeaviateKB(args.host, args.port, args.collection)
-        logger.info(f"initializing {args.collection}...")
+        conn = WeaviateKB(host, port, collection)
+        logger.info(f"initializing {collection}...")
     except:
         logger.error(f"couldn't connect to knowledgebase[{WeaviateKB.__name__}]")
         sys.exit(1)
@@ -102,7 +83,7 @@ if __name__ == "__main__":
 
 
 
-    for x in args.files:
+    for x in files:
 
         if not path.isdir("./.processing"):
             mkdir("./.processing")
@@ -111,7 +92,7 @@ if __name__ == "__main__":
             mkdir(proc_dir)
 
 
-        if True:
+        try:
             logger.info(f"initializing from {x}...")
 
             with TemporaryDirectory(dir=proc_dir, delete=False) as td:
@@ -126,20 +107,55 @@ if __name__ == "__main__":
                             False,
                             kb=conn
                         ),
-                    batch_size= args.batch_size if args.batch_size else estimate_chunks(x, args.designated_bytes, load=args.designated_load)
+                    batch_size= batch_size if batch_size else estimate_chunks(x, designated_bytes, load=designated_load)
                 )
             
             rmtree(td)
             succesful_reads.append(True)
 
 
-        # except Exception as e:
-        #     logger.error(f"could't read {x}: {str(e)}")
-        #     succesful_reads.append(False)
+        except Exception as e:
+            logger.error(f"could't read {x}: {str(e)}")
+            succesful_reads.append(False)
 
 
 
     #cli exection code
-    if succesful_reads and sum(succesful_reads) / len(succesful_reads) < args.error_threshold:
+    if succesful_reads and sum(succesful_reads) / len(succesful_reads) < error_threshold:
         logger.error(f"too many reads failed! ({sum(succesful_reads)}/{len(succesful_reads)})")
         sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    
+    #configuration
+    args = ArgumentParser("load")
+    args.add_argument("files", action="store", nargs="+", type=str)
+    args.add_argument("--host", action="store", type=str, required=True)
+    args.add_argument("--port", action="store", type=str, required=True)
+    args.add_argument("--collection", action="store", type=str, required=True)
+
+    args.add_argument("--batch_size", action="store", type=int, default=None)
+    args.add_argument("--designated_load", action="store", type=float, default=0.25)
+    args.add_argument("--designated_bytes", action="store", type=int, default=500000000)
+    args.add_argument("--error_threshold", action="store", type=float, default=0.8)
+    
+    args = args.parse_args()
+
+
+    load(
+        args.files,
+        args.host,
+        args.port,
+        args.collection,
+        args.batch_size,
+        args.designated_load,
+        args.designated_bytes,
+        args.error_threshold
+    )
+
+
+
+
+    
